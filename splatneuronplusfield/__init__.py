c0933987fb9160bfc5911db33e45d8df94a979ec
anttiluode <67085641+anttiluode@users.noreply.github.com>
2026-08-18 10:52:41 +0300

    Expose PlusField core API

---
+++ splatneuronplusfield/__init__.py
@@ -0,0 +1,11 @@
+"""SplatNeuronPlusField research instrument."""
+
+from .core import DualGeometry, SlowMilieu, induced_ephaptic, metric_green, metric_laplacian
+
+__all__ = [
+    "DualGeometry",
+    "SlowMilieu",
+    "induced_ephaptic",
+    "metric_green",
+    "metric_laplacian",
+]
